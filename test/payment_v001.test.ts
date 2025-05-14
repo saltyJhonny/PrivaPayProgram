c442d0f3b8e0471f8b57cce448594611d58c3781
saltyJhonny <degulama@proton.me>
2025-05-14 23:08:09 +0545

    feat: initialize project with package.json, deploy script, and TypeScript configuration
    - Added package.json for project dependencies and scripts.
    - Created privapay_v001.leo program with employee management functionalities.
    - Implemented deploy script for contract deployment.
    - Added test configuration to set up global console.
    - Created payment_v001.test.ts for testing payment contract functionalities.
    - Added TypeScript configuration file (tsconfig.json) for compiler options.

---
+++ test/payment_v001.test.ts
@@ -0,0 +1,93 @@
+import { ExecutionMode, js2leo } from "@doko-js/core";
+import { Payment_v001Contract } from "../artifacts/js/payment_v001";
+
+const mode = ExecutionMode.SnarkExecute;
+
+const paymentV001 = new Payment_v001Contract({mode: mode});
+
+
+const TIMEOUT = 300000_000;
+const company_id = BigInt(1);
+const company_name = BigInt(1234);
+
+
+
+
+
+describe("Payment Contract Test", () => {
+
+    const [aleouser1, aleouser2, aleouser3, aleoUser4] = paymentV001.getAccounts();
+
+    const company1_Metatadata = {
+    company_id: company_id,
+    company_name: company_name,
+    admin: aleouser1
+    }
+
+    test("deploy", async () => {
+        const deployTx = await paymentV001.deploy();
+        await deployTx.wait()
+    }, TIMEOUT);
+
+    test("Register company", async () => {
+        const tx = await paymentV001.register_company(company_id, company_name);
+        await tx.wait();
+        expect(paymentV001.registered_company(company_id)).toEqual(company1_Metatadata);
+    }, TIMEOUT);
+
+
+    describe("update company", () => {
+        const new_company_name = BigInt(5678);
+
+        const new_company_Metatadata = {
+            company_id: company_id,
+            company_name: new_company_name,
+            admin: aleouser1
+        }
+
+        test("only company admin can Update company data", async () => {
+            paymentV001.connect(aleoUser4);
+            const tx = await paymentV001.update_company(company_id, new_company_name, aleouser1);
+            await expect(tx.wait()).rejects.toThrow();
+        }, TIMEOUT);
+
+        test("Update company happy flow", async () => {
+            paymentV001.connect(aleouser1);
+            const tx = await paymentV001.update_company(company_id, new_company_name, aleouser1);
+            await tx.wait();
+            expect(paymentV001.registered_company(company_id)).toEqual(company1_Metatadata);
+        }, TIMEOUT);
+    });
+
+
+    describe("update company", () => {
+        const new_company_name = BigInt(5678);
+
+        const new_company_Metatadata = {
+            company_id: company_id,
+            company_name: new_company_name,
+            admin: aleouser1
+        }
+
+        test("only company admin can Update company data", async () => {
+            paymentV001.connect(aleoUser4);
+            const tx = await paymentV001.update_company(company_id, new_company_name, aleouser1);
+            await expect(tx.wait()).rejects.toThrow();
+        }, TIMEOUT);
+
+        test("Update company happy flow", async () => {
+            paymentV001.connect(aleouser1);
+            const tx = await paymentV001.update_company(company_id, new_company_name, aleouser1);
+            await tx.wait();
+            expect(paymentV001.registered_company(company_id)).toEqual(company1_Metatadata);
+        }, TIMEOUT);
+    });
+
+
+
+
+    
+
+    test("deploy", async () => {}, TIMEOUT);
+
+});
